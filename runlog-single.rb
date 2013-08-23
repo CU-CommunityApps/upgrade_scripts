ef0cf26fc4f5f30febed30a1f8afe9fd537e5be6
sbower <shawn.bower@gmail.com>
2013-08-23 14:29:14 -0400

    fix paths

--- runlog-single.rb
+++ runlog-single.rb
@@ -18,7 +18,7 @@ LIQUIBASE_1_9_5_CMD = "java -jar #{CURRENT_DIR}/lib/liquibase-1.9.5.jar --logLev
 LIQUIBASE_2_0_5_CMD = "java -jar #{CURRENT_DIR}/lib/liquibase-2.0.5.jar --logLevel=info #{CLASSPATH} --driver=oracle.jdbc.OracleDriver #{DBURL} #{USER} #{PASS} --changeLogFile=LOGFILE update"
 LIQUIBASE_3_0_2_CMD = "java -jar #{CURRENT_DIR}/lib/liquibase-3.0.2.jar --logLevel=info #{CLASSPATH} --driver=oracle.jdbc.OracleDriver #{DBURL} #{USER} #{PASS} --changeLogFile=LOGFILE update"
                   
-lb_cmd =  LIQUIBASE_3_0_2_CMD
+lb_cmd =  LIQUIBASE_2_0_5_CMD
 Dir.chdir(folder) do
   puts  "*" * 25 + "\n"
   puts "working in #{Dir.pwd}\n\n"
