56c9043ae24519fc0e7c03cd4de3595df6908aaa
sbower <shawn.bower@gmail.com>
2013-08-28 11:31:11 -0400

    KFSUPGRADE-114: added new upgrade script from Chad

--- runlog-cyn.rb
+++ runlog-cyn.rb
@@ -48,7 +48,8 @@ folders = [ {:name => "kfs/3.0.1_4.0", :version => 1, :changelogs => ['rice/kns_
                                                              '213_to_221_update_final_oracle.xml',
                                                              '221_to_222_update_final_oracle.xml',
                                                              '221_to_222_cleanup_final_oracle.xml',
-                                                             '223_to_224_update_final_oracle.xml'
+                                                             '223_to_224_update_final_oracle.xml',
+                                                             '22x_to_230_update_final_oracle.xml'
                                                              ]},
             
             {:name => "kfs/4.1.1_5.0", :version => 2, :changelogs => [#'rice_server/rice-server-script.xml',
