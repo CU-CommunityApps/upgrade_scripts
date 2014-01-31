7b5d2084a5f4548e8f1d3f5281d29bf56a0b4c9a
se10 <se10@cornell.edu>
2014-01-31 09:18:15 -0500

    KDO-483

--- runlog-cyn.rb
+++ runlog-cyn.rb
@@ -29,7 +29,8 @@ custom_changelogs = ['krim_rsp_t_update.xml',
                      'krim_role_permt_t_update.xml',
                      'parm_routing_update.xml',
                      'reindex_docs.xml',
-                     'clear_ksb.xml']
+                     'clear_ksb.xml',
+                     'krlc_cntry_t_update.xml']
 
 # folders in the correct order
 folders = [ {:name => "kfs/3.0.1_4.0", :version => 1, :changelogs => ['rice/kns_upgrade.xml', 
