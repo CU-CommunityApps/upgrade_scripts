c97daed49568c1e0e5d6b4bd3844f0caecd5d38a
Chad Bower <chad.bower379@gmail.com>
2014-03-04 17:20:22 -0500

    Adding script for account document conversion

---
+++ MaintDocConv/acctConv.rb
@@ -0,0 +1,194 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'oci8'
+
+
+
+require 'xmlsimple'
+require 'base64'
+require 'fast-aes'
+require 'pp'
+require 'prettyprint'
+require "rexml/document"
+#require "xpath"
+include REXML
+
+
+kfsdb = ARGV[0]
+kfs_pass = ARGV[1]
+cyndb = ARGV[2]
+cyn_pass = ARGV[3]
+key = ARGV[4]
+KEY = key.ljust(16)
+
+def decrypt(encoded_data)
+  data = Base64.decode64(encoded_data)
+  aes = FastAES.new(KEY)
+  
+  aes.decrypt(data)
+end
+
+def encrypt(unencoded_data)
+  aes = FastAES.new(KEY)
+
+  data = aes.encrypt(unencoded_data)
+  Base64.encode64(data)
+ 
+end
+
+
+conn = OCI8.new("kfs", kfs_pass, kfsdb)
+conn2 = OCI8.new("cynergy", cyn_pass, cyndb)
+
+docIds = Array.new
+
+cursor2 = conn2.exec("select DOC_HDR_ID from KREW_DOC_HDR_T where DOC_TYP_ID = '1849556' and DOC_HDR_STAT_CD = 'R'")
+
+while r2 = cursor2.fetch()
+  docIds.push(r2)
+end
+
+docId = ""
+
+docIds.each do |docid|
+  sdocid = docid.to_s
+  puts sdocid
+
+
+cursor = conn.exec("SELECT DOC_CNTNT from KRNS_MAINT_DOC_T WHERE
+                      DOC_HDR_ID = '#{sdocid}'")
+
+
+r = cursor.fetch_hash()
+
+
+good = decrypt(r['DOC_CNTNT'].read)
+good.gsub!("><", ">zz1<")
+
+doc_contnt = XmlSimple.xml_in(good)
+
+lbrCodeOld = doc_contnt['oldMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['extension'][0]['laborBenefitRateCategoryCode']
+lbrCodeNew = doc_contnt['newMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['extension'][0]['laborBenefitRateCategoryCode']
+
+newICRAcct = doc_contnt['newMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['indirectCostRecoveryAcctNbr']
+oldICRAcct = doc_contnt['oldMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['indirectCostRecoveryAcctNbr']
+newICRCd = doc_contnt['newMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['indirectCostRcvyFinCoaCode']
+oldICRCd = doc_contnt['oldMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['indirectCostRcvyFinCoaCode']
+newaccountNumber = doc_contnt['newMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['accountNumber']
+oldaccountNumber = doc_contnt['oldMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['accountNumber']
+
+oldICRAccts = Element.new "indirectCostRecoveryAccounts"
+oldICRAccts.attributes["class"] = "org.apache.ojb.broker.core.proxy.ListProxyDefaultImpl"
+newICRAccts = Element.new "indirectCostRecoveryAccounts"
+newICRAccts.attributes["class"] = "org.apache.ojb.broker.core.proxy.ListProxyDefaultImpl"
+
+purpOld = doc_contnt['oldMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['accountGuideline'][0]['accountPurposeText']
+purpNew = doc_contnt['newMaintainableObject'][0]['org.kuali.kfs.coa.businessobject.Account'][0]['accountGuideline'][0]['accountPurposeText']
+
+oldAcctNum = Element.new "accountNumber"
+oldAcctNum.text = oldaccountNumber
+newAcctNum = Element.new "accountNumber"
+newAcctNum.text = newaccountNumber
+
+oldICRAcctNum = Element.new "indirectCostRecoveryAccountNumber"
+oldICRAcctNum.text = oldICRAcct
+newICRAcctNum = Element.new "indirectCostRecoveryAccountNumber"
+newICRAcctNum.text = newICRAcct
+
+oldICRFCd = Element.new "indirectCostRecoveryFinCoaCode"
+oldICRFCd.text = oldICRCd
+newICRFCd = Element.new "indirectCostRecoveryFinCoaCode"
+newICRFCd.text = newICRCd
+
+lbrElOld = Element.new "laborBenefitRateCategoryCode"
+lbrElOld.text = lbrCodeOld
+
+lbrElNew = Element.new "laborBenefitRateCategoryCode"
+lbrElNew.text = lbrCodeNew
+
+version = Element.new "versionNumber"
+version.text = "1"
+
+xml = XmlSimple.xml_out(doc_contnt)
+
+
+doc = REXML::Document.new good
+
+XPath.each( doc, "//laborBenefitRateCategoryCode") { |element| element.text ="ZZ" }
+XPath.each( doc, "//accountGuideline") { |element| element.add_element version }
+
+
+  if newICRAcct.nil?
+    puts "No ICR"
+    newAcct = XPath.first( doc, "//newMaintainableObject/org.kuali.kfs.coa.businessobject.Account" )
+    newAcct.add_element lbrElNew
+    newAcct.add_element newICRAccts
+  else
+    newAcct = XPath.first( doc, "//newMaintainableObject/org.kuali.kfs.coa.businessobject.Account" )
+    newAcct.add_element lbrElNew
+    newAcct.add_element newICRAccts
+    newICR = XPath.first( doc, "//org.kuali.kfs.coa.businessobject.Account/indirectCostRecoveryAccounts" )
+    newICR.add_element newAcctNum
+    newICR.add_element newICRAcctNum
+    newICR.add_element newICRFCd
+  end 
+
+if oldICRAcct.nil?
+  puts "No old ICR"
+  if lbrCodeOld.nil?
+    puts "No old labor benefit cat code"
+  else
+    oldAcct = XPath.first( doc, "//oldMaintainableObject/org.kuali.kfs.coa.businessobject.Account" )
+    oldAcct.add_element lbrElOld
+    oldAcct.add_element oldICRAccts
+  end
+else
+  oldAcct = XPath.first( doc, "//oldMaintainableObject/org.kuali.kfs.coa.businessobject.Account" )
+  oldAcct.add_element lbrElOld
+  oldAcct.add_element oldICRAccts
+  oldICR = XPath.first( doc, "//org.kuali.kfs.coa.businessobject.Account/indirectCostRecoveryAccounts" )
+  oldICR.add_element oldAcctNum
+  oldICR.add_element oldICRAcctNum
+  oldICR.add_element oldICRFCd
+end
+
+docString = ""
+doc.write(docString)
+
+docString.gsub!("<laborBenefitRateCategoryCode>#{lbrCodeNew}</laborBenefitRateCategoryCode>", "  <laborBenefitRateCategoryCode>#{lbrCodeNew}</laborBenefitRateCategoryCode>\n")          
+
+if lbrCodeNew.eql? lbrCodeOld 
+puts "The labor ben rate codes are equal"
+else
+docString.gsub!("<laborBenefitRateCategoryCode>#{lbrCodeOld}</laborBenefitRateCategoryCode>", "  <laborBenefitRateCategoryCode>#{lbrCodeOld}</laborBenefitRateCategoryCode>\n")
+end
+
+docString.gsub!("<indirectCostRecoveryAccounts class='org.apache.ojb.broker.core.proxy.ListProxyDefaultImpl'>", "  <indirectCostRecoveryAccounts class='org.apache.ojb.broker.core.proxy.ListProxyDefaultImpl'>\n")
+
+docString.gsub!("<indirectCostRecoveryAccounts class='org.apache.ojb.broker.core.proxy.ListProxyDefaultImpl'/>", "<indirectCostRecoveryAccounts class='org.apache.ojb.broker.core.proxy.ListProxyDefaultImpl'/>\n")
+
+docString.gsub!("<accountNumber>#{newaccountNumber}</accountNumber><indirectCostRecoveryAccountNumber>#{newICRAcct}</indirectCostRecoveryAccountNumber><indirectCostRecoveryFinCoaCode>#{newICRCd}</indirectCostRecoveryFinCoaCode></indirectCostRecoveryAccounts></org.kuali.kfs.coa.businessobject.Account>", "    <org.kuali.kfs.coa.businessobject.IndirectCostRecoveryAccount>\n      <chartOfAccountsCode>IT</chartOfAccountsCode>\n      <accountNumber>#{newaccountNumber}</accountNumber>\n      <indirectCostRecoveryAccountNumber>#{newICRAcct}</indirectCostRecoveryAccountNumber>\n      <indirectCostRecoveryFinCoaCode>#{newICRCd}</indirectCostRecoveryFinCoaCode>\n      <accountLinePercent>100</accountLinePercent>\n      <active>true</active>\n      <newCollectionRecord>false</newCollectionRecord>\n    </org.kuali.kfs.coa.businessobject.IndirectCostRecoveryAccount>\n  </indirectCostRecoveryAccounts>\n</org.kuali.kfs.coa.businessobject.Account>")
+
+if ((oldaccountNumber.eql? newaccountNumber) && (oldICRAcct.eql? newICRAcct) && (oldICRCd.eql? newICRCd))
+  puts "ICR values are equal"
+else
+docString.gsub!("<accountNumber>#{oldaccountNumber}</accountNumber><indirectCostRecoveryAccountNumber>#{oldICRAcct}</indirectCostRecoveryAccountNumber><indirectCostRecoveryFinCoaCode>#{oldICRCd}</indirectCostRecoveryFinCoaCode></indirectCostRecoveryAccounts></org.kuali.kfs.coa.businessobject.Account>", "    <org.kuali.kfs.coa.businessobject.IndirectCostRecoveryAccount>\n      <chartOfAccountsCode>IT</chartOfAccountsCode>\n      <accountNumber>#{oldaccountNumber}</accountNumber>\n      <indirectCostRecoveryAccountNumber>#{oldICRAcct}</indirectCostRecoveryAccountNumber>\n      <indirectCostRecoveryFinCoaCode>#{oldICRCd}</indirectCostRecoveryFinCoaCode>\n      <accountLinePercent>100</accountLinePercent>\n      <active>true</active>\n      <newCollectionRecord>false</newCollectionRecord>\n    </org.kuali.kfs.coa.businessobject.IndirectCostRecoveryAccount>\n  </indirectCostRecoveryAccounts>\n</org.kuali.kfs.coa.businessobject.Account>")
+end
+
+docString.gsub!(">zz1<", "><")
+newDoc = encrypt(docString)
+newDoc.gsub!("\n", "")
+
+
+clob = OCI8::CLOB.new(conn, newDoc)
+
+
+cursor = conn.parse("UPDATE KRNS_MAINT_DOC_T SET DOC_CNTNT=(:1) WHERE DOC_HDR_ID = '#{sdocid}'")
+cursor.bind_param(1, clob, OCI8::CLOB, clob.size)
+cursor.exec()
+cursor.close()
+
+conn.commit
+
+end
