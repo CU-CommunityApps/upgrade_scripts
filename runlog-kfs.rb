9af019658853c21c63e468699118214a6f2b7ea0
sbower <shawn.bower@gmail.com>
2014-07-17 13:35:26 -0400

    KFSUPGRADE-987 #close #comment this has been added to the nightly upgrade scripts

--- runlog-kfs.rb
+++ runlog-kfs.rb
@@ -43,7 +43,7 @@ folders = [ {:name => "kfs/3.0.1_4.0", :version => 1, :changelogs => kfs_changel
             {:name => "custom/kfs/access_security", :version => 3, :changelogs => ['access-security-customizations.xml'] },
             {:name => "custom/kfs/vnd", :version => 3, :changelogs => ['vendor_extension.xml', 'delete_vendor_type.xml', 'chapter4_status_code.xml'] },
             {:name => "custom/kfs/fp", :version => 3, :changelogs => ['capital_asset_conversion.xml'] },
-            {:name => "custom/kfs/purap", :version => 3, :changelogs => ['app_doc_status.xml', 'app_pmt_rqst_itm_tx.xml', 'purap_po_v.xml'] },
+            {:name => "custom/kfs/purap", :version => 3, :changelogs => ['app_doc_status.xml', 'app_pmt_rqst_itm_tx.xml', 'purap_po_v.xml', 'delete_item_type.xml'] },
             {:name => "custom/kfs/gl", :version => 3, :changelogs => ['iaa-offset-defn.xml'] },
             {:name => "custom/kfs/purap", :version => 3, :changelogs => ['fs_doc_header_t_update.xml'] },
           ]
